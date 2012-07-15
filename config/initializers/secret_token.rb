68323c458b038822e64b7b1f0a10bb3ce13d7a30
Jason Sommer <jasonsommer95@gmail.com>
2012-07-14 23:08:03 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noti::Application.config.secret_token = 'ee7cd5b8fe9994cef23dae1aead051273e60e604082ace561cb0e0aea6307b9b7484291f5d1ad25d16eba988654f470914b34aeffc53444812d9c2e1c02bb5ae'
